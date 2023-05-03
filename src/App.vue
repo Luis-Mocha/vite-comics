d26daced4ef6ae4b41fb31f42edd506811a80192
Luis-Mocha <luismocha97@gmail.com>
2023-05-03 20:28:38 +0200

    headerComp: struttura inziale

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
-<script setup>
+<script>
 // import HelloWorld from './components/HelloWorld.vue'
+  import HeaderComp from './components/HeaderComp.vue'
+
+  export default{
+    name: 'App',
+    components: {
+      HeaderComp,
+    }
+  }
 
 </script>
 
 <template>
   <h1>prova prova</h1>
+  <HeaderComp/>
+  
 </template>
 
 <style scoped>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,102 @@
+<script>
+    export default{
+        name: "HeaderComp",
+        data(){
+            return {
+                menuLinks: [
+                    {
+                        nome: 'characters',
+                        url: '/',
+                        current: false
+                    },
+                    {
+                        nome: 'comics',
+                        url: '/',
+                        current: true
+                    },
+                    {
+                        nome: 'movies',
+                        url: '/',
+                        current: false
+                    },
+                    {
+                        nome: 'tv',
+                        url: '/',
+                        current: false
+                    },
+                    {
+                        nome: 'games',
+                        url: '/',
+                        current: false
+                    },
+                    {
+                        nome: 'collectibles',
+                        url: '/',
+                        current: false
+                    },
+                    {
+                        nome: 'videos',
+                        url: '/',
+                        current: false
+                    },
+                    {
+                        nome: 'fans',
+                        url: '/',
+                        current: false
+                    },
+                    {
+                        nome: 'news',
+                        url: '/',
+                        current: false
+                    },
+                    {
+                        nome: 'shop',
+                        url: '/',
+                        current: false
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<template>
+    <header>
+        <img src="/img/dc-logo.png" alt="">
+
+        <nav>
+            <ul>
+                <li v-for="(elem, index) in menuLinks" :key="index">
+                    <a :class="( elem.current === true ) ? 'active' : '' " href="">
+                        {{ elem.nome }}
+                    </a>
+                </li>
+                
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped lang="scss">
+    @use '../style/main.scss';
+
+    header{
+        display: flex;
+
+        nav {
+            display: block;
+
+            ul{
+                display: flex;
+                // margin: 0;
+                // padding: 0;
+
+                li {
+                    list-style-type: none;
+                }
+            }
+            
+        }
+    }
+     
+</style>
